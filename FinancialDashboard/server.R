86ac5943e9452d2222faab37f36e96799f3ec576
ismith1024 <ismith1024@gmail.com>
2018-06-20 19:21:42 -0400

    sidebar and valueboxes

--- FinancialDashboard/server.R
+++ FinancialDashboard/server.R
@@ -7,26 +7,10 @@ library(sqldf)
 require(stats)
 require(lattice)
 
-# Define server logic required to draw a histogram
-#shinyServer(function(input, output) {
-   
-#  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-#    x    <- faithful[, 2] 
-#    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-#    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-#  })
-  
-#})
-
 db <- dbConnect(SQLite(), dbname="~/Data/CADFinance.db")
 
 #####
-# getAll
+# getIntegrated
 #
 # returns a dataframe which contains all the information:
 # date
@@ -35,8 +19,6 @@ db <- dbConnect(SQLite(), dbname="~/Data/CADFinance.db")
 # annualized return
 # p-e
 # div yield
-
-#TODO: count the non-null close dates an put into proper data frame (asfer counting the PE and DIV)
 getAll <- function(symb){
   print("GetAll()")
   
@@ -129,11 +111,9 @@ getAll <- function(symb){
     
   }
   
-  print(finalVals)
+  #print(finalVals)
   
-  #TODO: this is still a mess
-  #need to put the filtered time series into the middle of the data frame
-  #smooth the time series
+  #TODO: smoothed time series of same length as data frame
   #generate the kernel from here for now:
   #http://dev.theomader.com/gaussian-kernel-calculator/
   
@@ -143,36 +123,11 @@ getAll <- function(symb){
   #populate the annualized return
   #x[[i, "annualized"]] <-getAnnualizedReturn(x[[i, "day_"]], x[[i, month_]], x[[i, year]], x[[i, "close"]], finalDay, finalMonth, finalYear, finalVal)
   
-  
-  dv.mod1 <- lm(ann ~ pe, data = finalVals)
-  summary(dv.mod1)
-  plot(finalVals$pe, finalVals$ann, main = paste("P-E Regression: ", symb, sep = ""), xlab = "P-E", ylab = "Annualized Return")
-  abline(dv.mod1)
-  
-  
-  dv.mod2 <- lm(ann ~ div, data = finalVals)
-  summary(dv.mod2)
-  plot(finalVals$div, finalVals$ann, main = paste("Dividend Regression: ", symb, sep = ""), xlab = "Dividend Yield", ylab = "Annualized Return")
-  abline(dv.mod2)
-  
   return(finalVals)
 }
 
 
-#get the time series
-getTimeSeries <- function(symb){
-  sqlQuery <- paste("SELECT close, day_, month_, year_ FROM xtse WHERE symbol = '", symb, "';", sep = "")
-  #print(sqlQuery)
-  rs <- dbSendQuery(db, sqlQuery) #time series
-  while (!dbHasCompleted(rs)) {
-    values <- dbFetch(rs)
-  }
-  
-  timeSeries <- values
-  dbClearResult(rs)
-  return(timeSeries)
-  
-}
+
 
 # provides the annualized return of the equity to the most recent date on record
 # (today's value / initial value)^(1/time); time in years
@@ -186,64 +141,6 @@ getAnnualizedReturn <- function(initDay, initMonth, initYear, initVal, finalDay,
   return(change)
 }
 
-
-
-getIntegrated <- function(symb){
-  sqlQuery <- paste("SELECT DISTINCT day_, month_, year_, close, eps, div FROM (SELECT day_, month_, year_ FROM xtse WHERE symbol = '", symb, "' UNION all SELECT 15 as day_, month_, year_ FROM earnings WHERE symbol = '", symb, "') LEFT JOIN( SELECT 15 as day2, month_ as month2, year_ as year2, div, eps FROM earnings WHERE symbol = '", symb, "') ON day_ = day2 AND month_ = month2 AND year_ = year2 LEFT JOIN ( SELECT day_ as day3, month_ as month3, year_ as year3, close FROM xtse WHERE symbol = '", symb, "') ON day_ = day3 AND month_ = month3 AND year_ = year3 ORDER BY year_, month_, day_;", sep = "")
-  print(sqlQuery)
-  rs <- dbSendQuery(db, sqlQuery) 
-  
-  while (!dbHasCompleted(rs)) {
-    values <- dbFetch(rs)
-    print(dbFetch(rs))
-  }
-  
-  ret <- values
-  
-  #counts the number of valid data points we have for earnings
-  earnsCount <- 0
-  
-  #track the last four eps and div
-  earns <- c(0,0,0,0)
-  divs <- c(0,0,0,0)
-  anEPS <- 0.0
-  anDiv <- 0.0
-  
-  for(i in 1:nrow(ret)){
-    if(!is.na(ret[[i, "eps"]])){
-      earnsCount <- earnsCount + 1
-      earns[1] <- earns[2]
-      earns[2] <- earns[3]
-      earns[3] <- earns[4]
-      earns[4] <- ret[i, "eps"]
-      anEPS <- earns[1] + earns[2] + earns[3] + earns[4]
-      
-      divs[1] <- divs[2]
-      divs[2] <- divs[3]
-      divs[3] <- divs[4]
-      divs[4] <- ret[i, "div"]
-      anDiv <- divs[1] + divs[2] + divs[3] + divs[4]
-      
-      #print(paste("EPS: ", anEPS, " Div: ", anDiv, sep = ""))
-    }
-    
-    if(!is.na(ret[i, "close"]) && earnsCount >= 4){
-      ret[i, "pe"] <- ret[i, "close"] / anEPS
-      ret[i, "divYld"] <- 100 * anDiv / ret[i, "close"] 
-    }
-  }
-  
-  #summary(dv.mod1)
-  #dv.mod1 <- lm(divYld ~ pe, data = ret)
-  #plot(ret$divYld, ret$pe, main = paste("Demo Regression: ", symb, sep = ""), xlab = "Dividend Yield", ylab = "P-E")
-  
-  #abline(dv.mod1)
-  
-  
-  return(ret)
-  
-}
-
 runInt <- function(symb){
   intData <- getIntegrated(symb)
   intData
@@ -254,11 +151,7 @@ runInt <- function(symb){
 shinyServer(function(input, output) {
   
   print("Starting server")
-  
-  all <- getAll("BNS")
-  
-  print(all)
-  
+
   updateSymbol<-reactive({
     if(exists(input$symInp))
       input$symInp
@@ -267,13 +160,19 @@ shinyServer(function(input, output) {
   })
   
   observeEvent(input$update_button, {
+    peProj <- 0.0
+    divProj <- 0.0
+
+    
     #update data
     print(paste("New symbol ", input$symInp))
     sym <- input$symInp
-    dataSer <- runInt(sym)
+    dataSer <- getAll(sym)
+    kern = c(0,0.000001,0.000002,0.000005,0.000012,0.000027,0.00006,0.000125,0.000251,0.000484,0.000898,0.001601,0.002743,0.004514,0.00714,0.010852,0.015849,0.022242,0.029993,0.038866,0.048394,0.057904,0.066574,0.073551,0.078084,0.079656,0.078084,0.073551,0.066574,0.057904,0.048394,0.038866,0.029993,0.022242,0.015849,0.010852,0.00714,0.004514,0.002743,0.001601,0.000898,0.000484,0.000251,0.000125,0.00006,0.000027,0.000012,0.000005,0.000002,0.000001,0)
+    wTran <- convolve(dataSer[["close"]], kern, type = "filter")
     print(paste("New symbol ", sym))
     output$pricePlot <- renderPlot({
-      dataSer <- getIntegrated(sym)
+      #dataSer <- getIntegrated(sym)
       
       #ts <- runTS(sym)
       #get the time series
@@ -286,8 +185,11 @@ shinyServer(function(input, output) {
       }
     })
     
+    currentVal <- tail(wTran, x = 1)
+    todaysDiv <- tail(dataSer[["div"]], 1)
+    todaysPE <- tail(dataSer[["pe"]], 1)
+    
     output$smoothPlot <- renderPlot({
-      wTran <- runTrans(sym)
       #get the smoothed time series
       input$update_button
       
@@ -298,23 +200,57 @@ shinyServer(function(input, output) {
     })
     
     output$retPlot <- renderPlot({
-      retSer <- runRets(sym)
-      if(!is.null(retSer)){
-        plot(retSer, pch = ".")
+      #retSer <- runRets(sym)
+      if(!is.null(dataSer[["ann"]])){
+        plot(dataSer[["ann"]], pch = ".")
       }
     })
     
     output$divPlot <- renderPlot({
-      if(!is.null(dataSer[["divYld"]])){
-        plot(dataSer[["divYld"]], pch = ".")
+      if(!is.null(dataSer[["div"]])){
+        todaysDiv <- tail(dataSer[["div"]], 1)
+        todaysPE <- tail(dataSer[["pe"]], 1)
+                #plot(dataSer[["divYld"]], pch = ".")
+        dv.mod2 <- lm(ann ~ div, data = dataSer)
+        summary(dv.mod2)
+        plot(dataSer$div, dataSer$ann, xlab = "Dividend Yield", ylab = "Annualized Return")
+        abline(dv.mod2)
+        divProj <<- predict(dv.mod2, data.frame(div = c(todaysDiv)))
       }
+
     })
+    
+    print(paste("PE PROJ: ", peProj, sep = ""))
     
     output$pePlot <- renderPlot({
       if(!is.null(dataSer[["pe"]])){
-        plot(dataSer[["pe"]], pch = ".")
+        todaysDiv <- tail(dataSer[["div"]], 1)
+        todaysPE <- tail(dataSer[["pe"]], 1)
+        #plot(dataSer[["pe"]], pch = ".")
+        dv.mod1 <- lm(ann ~ pe, data = dataSer)
+        summary(dv.mod1)
+        plot(dataSer$pe, dataSer$ann, xlab = "P-E", ylab = "Annualized Return")
+        abline(dv.mod1)
+        divProj <<- predict(dv.mod1, data.frame(pe = c(todaysPE)))
       }
     })
+    
+    print(paste("DIV PROJ: ", divProj, sep = ""))
+    
+    output$divProjBox <- renderValueBox({
+      valueBox(
+        divProj, "Dividend Projection", icon = icon("list"),
+        color = "purple"
+      )
+    })
+    
+    output$peProjBox <- renderValueBox({
+      valueBox(
+        peProj, "P-E Projection", icon = icon("list"),
+        color = "yellow"
+      )
+    })
+    
     
   })
   
@@ -324,98 +260,120 @@ shinyServer(function(input, output) {
     }
   }
   
-  dataSer <- getIntegrated("BNS")
+  dataSer <- getAll("BNS")
   
   output$pricePlot <- renderPlot({
-    ts <- runTS("BNS")
-    if(!is.null(ts)){
-      plot(ts, pch = ".")
+
+    if(!is.null(dataSer)){
+      plot(dataSer[["close"]], pch = ".")
     }
   })
   
   output$smoothPlot <- renderPlot({
-    wTran <- runTrans("BNS")
+    kern <- c(0,0.000001,0.000002,0.000005,0.000012,0.000027,0.00006,0.000125,0.000251,0.000484,0.000898,0.001601,0.002743,0.004514,0.00714,0.010852,0.015849,0.022242,0.029993,0.038866,0.048394,0.057904,0.066574,0.073551,0.078084,0.079656,0.078084,0.073551,0.066574,0.057904,0.048394,0.038866,0.029993,0.022242,0.015849,0.010852,0.00714,0.004514,0.002743,0.001601,0.000898,0.000484,0.000251,0.000125,0.00006,0.000027,0.000012,0.000005,0.000002,0.000001,0)
+    wTran <- convolve(dataSer[["close"]], kern, type = "filter")
+    
+    #wTran <- runTrans("BNS")
     if(!is.null(wTran)){
       plot(wTran, pch = ".")
     }
   })
   
   output$retPlot <- renderPlot({
-    retSer <- runRets("BNS")
-    if(!is.null(retSer)){
-      plot(retSer, pch = ".")
+    #retSer <- runRets("BNS")
+    if(!is.null(dataSer[["ann"]])){
+      plot(dataSer[["ann"]], pch = ".")
     }
   })
   
   output$divPlot <- renderPlot({
-    if(!is.null(dataSer[["divYld"]])){
-      plot(dataSer[["divYld"]], pch = ".")
+    if(!is.null(dataSer[["div"]])){
+      todaysDiv <- tail(dataSer[["div"]], 1)
+      todaysPE <- tail(dataSer[["pe"]], 1)
+      #plot(dataSer[["divYld"]], pch = ".")
+      dv.mod2 <- lm(ann ~ div, data = dataSer)
+      summary(dv.mod2)
+      plot(dataSer$div, dataSer$ann, xlab = "Dividend Yield", ylab = "Annualized Return")
+      abline(dv.mod2)
+      
+      divProj <<- predict(dv.mod2, data.frame(div = c(todaysDiv)))
     }
   })
   
   output$pePlot <- renderPlot({
+    todaysDiv <- tail(dataSer[["div"]], 1)
+    todaysPE <- tail(dataSer[["pe"]], 1)
     if(!is.null(dataSer[["pe"]])){
-      plot(dataSer[["pe"]], pch = ".")
+      #plot(dataSer[["pe"]], pch = ".")
+      dv.mod1 <- lm(ann ~ pe, data = dataSer)
+      summary(dv.mod1)
+      plot(dataSer$pe, dataSer$ann, xlab = "P-E", ylab = "Annualized Return")
+      abline(dv.mod1)
+      peProj <<- predict(dv.mod1, data.frame(pe = c(todaysPE)))
     }
   })
   
+  output$divProjBox <- renderValueBox({
+    valueBox(
+      divProj, "Dividend Projection", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  output$peProjBox <- renderValueBox({
+    valueBox(
+      peProj, "P-E Projection", icon = icon("list"),
+      color = "yellow"
+    )
+  })
   
 })
 
-#server <- function(input, output) { }
-
-#shinyApp(ui, server)
-
-
-############# SCRAP
-#get time series and compute the return data for a symbol
-#time series is smoothed using a Weiersrass transform -- intended to remove Gaussian noise
-#Kernel prameters:
-#n = 51
-#sigma = 10
-runRets <- function(symb){
-  timeSer <- getTimeSeries(symb)
-  #generate the kernel from here for now:
-  #http://dev.theomader.com/gaussian-kernel-calculator/
-  kern = c(0,0.000001,0.000002,0.000005,0.000012,0.000027,0.00006,0.000125,0.000251,0.000484,0.000898,0.001601,0.002743,0.004514,0.00714,0.010852,0.015849,0.022242,0.029993,0.038866,0.048394,0.057904,0.066574,0.073551,0.078084,0.079656,0.078084,0.073551,0.066574,0.057904,0.048394,0.038866,0.029993,0.022242,0.015849,0.010852,0.00714,0.004514,0.002743,0.001601,0.000898,0.000484,0.000251,0.000125,0.00006,0.000027,0.000012,0.000005,0.000002,0.000001,0)
-  wTrans = convolve(timeSer[["close"]], kern, type = "filter")
-    #plot(wTrans, pch = ".")
-  today <- tail(timeSer, 1)
+#getIntegrated <- function(symb){
+#  sqlQuery <- paste("SELECT DISTINCT day_, month_, year_, close, eps, div FROM (SELECT day_, month_, year_ FROM xtse WHERE symbol = '", symb, "' UNION all SELECT 15 as day_, month_, year_ FROM earnings WHERE symbol = '", symb, "') LEFT JOIN( SELECT 15 as day2, month_ as month2, year_ as year2, div, eps FROM earnings WHERE symbol = '", symb, "') ON day_ = day2 AND month_ = month2 AND year_ = year2 LEFT JOIN ( SELECT day_ as day3, month_ as month3, year_ as year3, close FROM xtse WHERE symbol = '", symb, "') ON day_ = day3 AND month_ = month3 AND year_ = year3 ORDER BY year_, month_, day_;", sep = "")
+#  print(sqlQuery)
+#  rs <- dbSendQuery(db, sqlQuery) 
   
-  fYear <- today[["year_"]]
-  fMon <- today[["month_"]]
-  fDay <- today[["day_"]]
-  fVal <- tail(wTrans,1)
-  rets <- vector("numeric", nrow(timeSer))
-
-   #for(i in 1:10){
-  for(i in 1:nrow(timeSer)){
-    iYear <- timeSer[i, "year_"]
-    iMon <- timeSer[i, "month_"]
-    iDay <- timeSer[i, "day_"]
-    iVal <- wTrans[i]
-    rets[i] <- getAnnualizedReturn(iDay, iMon, iYear, iVal, fDay, fMon, fYear, fVal)
+#  while (!dbHasCompleted(rs)) {
+#    values <- dbFetch(rs)
+#    print(dbFetch(rs))
+#  }
+  
+#  ret <- values
+  
+  #counts the number of valid data points we have for earnings
+#  earnsCount <- 0
+  
+  #track the last four eps and div
+#  earns <- c(0,0,0,0)
+#  divs <- c(0,0,0,0)
+#  anEPS <- 0.0
+#  anDiv <- 0.0
+  
+#  for(i in 1:nrow(ret)){
+#    if(!is.na(ret[[i, "eps"]])){
+#      earnsCount <- earnsCount + 1
+#      earns[1] <- earns[2]
+#      earns[2] <- earns[3]
+#      earns[3] <- earns[4]
+#      earns[4] <- ret[i, "eps"]
+#      anEPS <- earns[1] + earns[2] + earns[3] + earns[4]
+      
+#      divs[1] <- divs[2]
+#      divs[2] <- divs[3]
+#      divs[3] <- divs[4]
+#      divs[4] <- ret[i, "div"]
+#      anDiv <- divs[1] + divs[2] + divs[3] + divs[4]
+      
+      #print(paste("EPS: ", anEPS, " Div: ", anDiv, sep = ""))
+#    }
     
-  }
+#    if(!is.na(ret[i, "close"]) && earnsCount >= 4){
+#      ret[i, "pe"] <- ret[i, "close"] / anEPS
+#      ret[i, "divYld"] <- 100 * anDiv / ret[i, "close"] 
+#    }
+#  }
   
-#  print(today)
-#  plot(rets, pch = ".")
+#  return(ret)
   
-}
-
-runTS <- function(symb){
-  print(paste("Run TS: ", symb))
-  timeSer <- getTimeSeries(symb)
-  plot(timeSer[["close"]], pch = ".")
-  return(timeSer[["close"]])
-}
-
-runTrans <- function(symb){
-  timeSer <- getTimeSeries(symb)
-#generate the kernel from here for now:
-#http://dev.theomader.com/gaussian-kernel-calculator/
-  kern = c(0,0.000001,0.000002,0.000005,0.000012,0.000027,0.00006,0.000125,0.000251,0.000484,0.000898,0.001601,0.002743,0.004514,0.00714,0.010852,0.015849,0.022242,0.029993,0.038866,0.048394,0.057904,0.066574,0.073551,0.078084,0.079656,0.078084,0.073551,0.066574,0.057904,0.048394,0.038866,0.029993,0.022242,0.015849,0.010852,0.00714,0.004514,0.002743,0.001601,0.000898,0.000484,0.000251,0.000125,0.00006,0.000027,0.000012,0.000005,0.000002,0.000001,0)
-  wTrans = convolve(timeSer[["close"]], kern, type = "filter")
-  plot(wTrans, pch = ".")
-  return(wTrans)
-}
+#}
--- FinancialDashboard/ui.R
+++ FinancialDashboard/ui.R
@@ -7,43 +7,12 @@ library(sqldf)
 require(stats)
 require(lattice)
 
-# Define UI for application that draws a histogram
-#shinyUI(fluidPage(
-  
-  # Application title
-  #titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  #sidebarLayout(
-  #  sidebarPanel(
-  #     sliderInput("bins",
-  #                 "Number of bins:",
-  #                 min = 1,
-  #                 max = 50,
-  #                 value = 30)
-  #  ),
-    
-    # Show a plot of the generated distribution
-  #  mainPanel(
-  #     plotOutput("distPlot")
-  #  )ui <- dashboardPage(
-  #)
-#))
-
 #Dashboard header carrying the title of the dashboard
-header <- dashboardHeader(title = "Financial Analysis")  
-#Sidebar content of the dashboard
-sidebar <- dashboardSidebar(
-  sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Visit-us", icon = icon("send",lib='glyphicon'), 
-             href = "https://www.salesforce.com")
-  )
-)
 
 frow0 <- fluidRow(
-  textInput(inputId = "symInp", label = "Symbol", value = ""),
-  actionButton("update_button", "Update chart")
+  valueBoxOutput("divProjBox"),
+  
+  valueBoxOutput("peProjBox")
 )
 
 frow1 <- fluidRow(
@@ -100,7 +69,19 @@ frow4 <- fluidRow(
   ) 
 )
 
-# combine the two fluid rows to make the body
+header <- dashboardHeader(title = "Financial Analysis")  
+#Sidebar content of the dashboard
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    sidebarSearchForm(textId = "symInp", buttonId = "update_button", label = "Symbol")
+    #menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    #menuItem("Visit-us", icon = icon("send",lib='glyphicon'), href = "https://www.salesforce.com")
+    
+  )
+)
+
+
+# combine the fluid rows to make the body
 body <- dashboardBody(frow0, frow1, frow2, frow3, frow4)
 
 ui <- dashboardPage(
